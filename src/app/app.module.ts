89629d72fd7f2f4710c48bc9d0c0d95ef604c3b6
gisumit <gsumitingole.com@gmail.com>
2017-03-01 09:10:25 -0800

    step one complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ListingsComponent } from './listings/listings.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ListingComponent } from './listing/listing.component';
+import { AddListingComponent } from './add-listing/add-listing.component';
+import { EditListingComponent } from './edit-listing/edit-listing.component';
+
+
+const appRoutes : Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'listings', component: ListingsComponent},
+  {path: 'add-listing', component: AddListingComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ListingsComponent,
+    NavbarComponent,
+    ListingComponent,
+    AddListingComponent,
+    EditListingComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
